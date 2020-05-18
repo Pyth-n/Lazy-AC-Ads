6d4938a40085c66b09ff2006857e19e6a7233f33
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-18 14:48:46 -0700

    Begin to process text

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import math
-from PIL import Image, ImageFilter
+from PIL import Image, ImageDraw, ImageFont
 
 # get rid of previous debug clutter
 os.system('clear')
@@ -15,17 +15,51 @@ def openImages():
     # add image objects to dictionary
     for x in os.listdir(path):
         fullPath = os.path.join(path, x)
-        tmp = Image.open(fullPath)
-        images[fullPath] = tmp
+        # ignore folders
+        if os.path.isfile(fullPath):
+            tmp = Image.open(fullPath)
+            images[fullPath] = tmp
+
+def processText(indexes, text):
+    global draw, arialFont
+
+    x, y = None, None
+    print(f'{indexes}: {text}')
+    
+    if indexes[0] == 64:
+        x = 0
+    elif indexes[0] == 256:
+        x = 213
+    elif indexes[0] == 448:
+        x = 426
+    else:
+        raise Exception(ArithmeticError('x out of bounds'))
+
+    if indexes[1] == 0:
+        y = 120
+    elif indexes[1] == 192:
+        y = 310
+    elif indexes[1] == 384:
+        y = 500
+    else:
+        raise Exception(ArithmeticError('y out of bounds'))
+ 
+    draw.text((x, y), text, fill = 'white', font=arialFont)
+
+    print(f'Alignment: ({x},{y})')
 
 openImages()
 
 root = Image.new('RGBA', (640, 640))
+arialFont = ImageFont.truetype("assets/fonts/arialbd.ttf", 15)
+draw = ImageDraw.Draw(root)
 
+# drawing algorithm
 x = 64
 y = 0
-for key in images:
+for index, key in enumerate(images):
     root.paste(images[key], (x, y))
+    processText((x, y), 'WWWWWWWWWWWWWW')
     x += 192
 
     if x == 640:

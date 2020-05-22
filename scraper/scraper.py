17e1a441454e40a86c5846652e3246217c88a732
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-21 19:25:56 -0700

    Get image links. Refactor

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,20 +6,28 @@ opts = Options()
 opts.set_headless()
 assert opts.headless
 
-itemNames = []
+def getDescriptionsAndImages(link):
+    global browser
+    # opens the link. waits every DOM query
+    browser.get(link)
+    browser.implicitly_wait(100)
+    results = browser.find_elements_by_class_name('row')
+    browser.implicitly_wait(100)
+    children = results[0].find_elements_by_class_name('listing-row')
+    browser.implicitly_wait(100)
+
+    # stores description and image links in dictionary
+    for text in children:
+        browser.implicitly_wait(100)
+        description = text.find_element_by_xpath('.//div[@class="listing-name"]').text
+        imgSrc = text.find_element_by_xpath('.//img[@class="listing-item-img"]').get_attribute('src')
+        items[imgSrc] = description
+
+# Item dictionary
+items = {}
 
 browser = Firefox(executable_path='scraper/geckodriver')
-browser.get('https://nookazon.com/profile/1921469521/wishlist')
-browser.implicitly_wait(100)
 
-results = browser.find_elements_by_class_name('row')
-children = results[0].find_elements_by_class_name('listing-row')
-browser.implicitly_wait(20)
+getDescriptionsAndImages('https://nookazon.com/profile/1921469521/wishlist')
 
-# text
-for text in children:
-    lol = text.find_element_by_xpath('.//div[@class="listing-name"]').text
-    itemNames.append(lol)
-
-print(itemNames)
 browser.close()

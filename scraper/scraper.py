bbe106b72b3ac8d6daf0bf3217b209d742b622f5
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-21 19:29:16 -0700

    Refactor for readability

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,13 +6,20 @@ opts = Options()
 opts.set_headless()
 assert opts.headless
 
+# Item dictionary
+items = {}
+
+browser = Firefox(executable_path='scraper/geckodriver')
+
 def getDescriptionsAndImages(link):
     global browser
     # opens the link. waits every DOM query
     browser.get(link)
     browser.implicitly_wait(100)
+
     results = browser.find_elements_by_class_name('row')
     browser.implicitly_wait(100)
+    
     children = results[0].find_elements_by_class_name('listing-row')
     browser.implicitly_wait(100)
 
@@ -22,11 +29,6 @@ def getDescriptionsAndImages(link):
         description = text.find_element_by_xpath('.//div[@class="listing-name"]').text
         imgSrc = text.find_element_by_xpath('.//img[@class="listing-item-img"]').get_attribute('src')
         items[imgSrc] = description
-
-# Item dictionary
-items = {}
-
-browser = Firefox(executable_path='scraper/geckodriver')
 
 getDescriptionsAndImages('https://nookazon.com/profile/1921469521/wishlist')
 

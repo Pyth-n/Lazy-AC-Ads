ab8263aa596994ee6449622a41257e60a6442d06
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-23 18:02:50 -0700

    Implement main

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+if __name__ != '__main__':
+    print('Can\'t be imported')
+    quit()
+
 import sys
 import os
+import re
+
 from PIL import Image
 
-# TODO: implement main program
+from lazyacads.scraper import scraper
+from lazyacads.render import render
+
+re_url = re.compile(r'^(https://)?(nookazon.com/profile/\d*/wishlist)')
+
+url = 'https://nookazon.com/profile/1921469521/wishlist' #input('Enter/paste nookazon link: ')
+
+try:
+    re_url.search(url).group(2)
+except AttributeError as ae:
+    print(ae)
+    print('That link is not a wishlist!')
+    quit()
+
+print('Scraping website, please do NOT touch')
+scraper.main(url)
+
+print('Beginning to render file')
+render.main()

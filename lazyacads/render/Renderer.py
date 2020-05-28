0cb0cf6beb65d05f720b3e310fd98fcd6bf370d9
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-28 13:25:01 -0700

    Add private __openItems This function checks if the given path exists. It also makes sure it is a dictionary. If it is not, it will return an empty dictionary.

--- lazyacads/render/Renderer.py
+++ lazyacads/render/Renderer.py
@@ -8,15 +8,23 @@ import requests
 from PIL import Image, ImageDraw, ImageFont
 from lazyacads.scraper import scraper
 
-__defaultItemsPath = os.getcwd()
-
 class Renderer:
     def __init__(self, itemsPath: str) -> None:
         self.__images = {}
-        self.__items = {}
+        self.__items = self.__openItems(itemsPath)
         self.__itemsLocal = {}
         self.__shouldPromptPrice = False
         print(self)
+
+    # Private functions
+    def __openItems(self, itemsPath: str) -> dict:
+        # Verify path exists
+        try:
+            with open(itemsPath, 'rb') as f:
+                tmp = f.read()
+            return ast.literal_eval(tmp)
+        except OSError:
+            return {}
 
 if __name__ == '__main__':
     Renderer('lol')

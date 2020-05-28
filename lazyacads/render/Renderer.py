060a5accef7fef7180613d84504f244d67228b2a
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-05-28 14:19:11 -0700

    Add pathlib Path

--- lazyacads/render/Renderer.py
+++ lazyacads/render/Renderer.py
@@ -3,6 +3,7 @@ import sys
 import math
 import ast
 import time
+from pathlib import Path
 
 import requests
 from PIL import Image, ImageDraw, ImageFont
@@ -10,21 +11,23 @@ from lazyacads.scraper import scraper
 
 class Renderer:
     def __init__(self, itemsPath: str) -> None:
+        # private members
         self.__images = {}
         self.__items = self.__openItems(itemsPath)
         self.__itemsLocal = {}
         self.__shouldPromptPrice = False
-        print(self)
 
     # Private functions
     def __openItems(self, itemsPath: str) -> dict:
-        # Verify path exists
+        # Verify path opens a legit dictionary
         try:
-            with open(itemsPath, 'rb') as f:
+            with open(itemsPath, 'r') as f:
                 tmp = f.read()
             return ast.literal_eval(tmp)
         except OSError:
+            print('Items.txt file not found')
             return {}
 
 if __name__ == '__main__':
-    Renderer('lol')
+    test = Renderer(Path(os.getcwd(), 'lazyacads', 'scraper', 'items.txt'))
+    print(test)
